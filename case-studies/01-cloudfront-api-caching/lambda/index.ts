5acd36db16c4afaef9f90a174bedc2a8b397ca0e
Hong-Phat Nguyen <nhphat1997@gmail.com>
2025-03-22 14:01:35 +0700

    Restucture

--- case-studies/01-cloudfront-api-caching/lambda/index.ts
+++ case-studies/01-cloudfront-api-caching/lambda/index.ts
@@ -1,4 +1,7 @@
-export const handler = async(event) => {
+
+import { APIGatewayProxyEvent } from 'aws-lambda';
+
+export const handler = async(event: APIGatewayProxyEvent) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
     return {
         "statusCode": 200,
--- case-studies/app.ts
+++ case-studies/app.ts
@@ -2,4 +2,4 @@ import { App } from 'aws-cdk-lib';
 import { CachingExperients } from './01-cloudfront-api-caching/index';
 
 const app = new App();
-new CachingExperients(app, 'MyLambdaStack');
+new CachingExperients(app, 'CloudFrontAPICaching');
